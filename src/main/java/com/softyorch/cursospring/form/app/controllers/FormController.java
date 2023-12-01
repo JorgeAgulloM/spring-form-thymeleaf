ca8d71b370368bd441bf328f425119e050b87d6d
Jorge1 <agullojorge@gmail.com>
2023-12-01 06:44:23 +0100

    Controller created

---
+++ src/main/java/com/softyorch/cursospring/form/app/controllers/FormController.java
@@ -0,0 +1,22 @@
+package com.softyorch.cursospring.form.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class FormController {
+
+    @GetMapping("/form")
+    public String form(Model model) {
+        return "form";
+    }
+
+    @PostMapping("/form")
+    public String input(Model model) {
+        return "result";
+    }
+
+
+}
