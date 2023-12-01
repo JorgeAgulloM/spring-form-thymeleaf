14d0fff5523ef52199d2b69b5c714dc16b9c3530
Jorge1 <agullojorge@gmail.com>
2023-12-01 07:06:33 +0100

    Working on the controller

--- src/main/java/com/softyorch/cursospring/form/app/controllers/FormController.java
+++ src/main/java/com/softyorch/cursospring/form/app/controllers/FormController.java
@@ -4,17 +4,30 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FormController {
 
-    @GetMapping("/form")
+    @GetMapping({"/form", "/"})
     public String form(Model model) {
+        model.addAttribute("title", "Formulario");
         return "form";
     }
 
     @PostMapping("/form")
-    public String input(Model model) {
+    public String input(
+            Model model,
+            @RequestParam(name = "username") String username,
+            @RequestParam(name = "password") String password,
+            @RequestParam(name = "email") String email
+    ) {
+
+        model.addAttribute("title", "Resultado");
+        model.addAttribute("username", username);
+        model.addAttribute("password", password);
+        model.addAttribute("email", email);
+
         return "result";
     }
 
